37b0042b5d38f4fb010c6dc4a5ca9e70d9aee354
Dion <Digitalfortress20@gmail.com>
2024-09-30 15:05:05 +0800

    goods na.. focus na jud ta sa store

--- app/src/main/java/com/odessy/srlaundry/modify_sms.kt
+++ app/src/main/java/com/odessy/srlaundry/modify_sms.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 
 class modify_sms : AppCompatActivity() {
